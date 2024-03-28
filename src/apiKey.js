839caf02c1957a76d8787f9aa4f0a7b37ceb2b9c
Livingstone17 <peterson.omobolaji@gmail.com>
2024-03-28 19:15:53 +0100

    adding weather funtionality and updating package.json

---
+++ src/apiKey.js
@@ -0,0 +1,6 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "6b4c43e4ac457dbb392fcfd2792f1766",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
+  
--- src/myLocation.js
+++ src/myLocation.js
@@ -1,7 +1,155 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import apiKey from "./apiKey";
+import Clock from "react-live-clock";
+import Forecast from "./forecast";
+import loader from "./images/WeatherIcons.gif";
+import ReactAnimatedWeather from "react-animated-weather";
 
-export default function MyLocation() {
-  return (
-    <div>myLocation</div>
-  )
-}
+const dateBuilder = (d) => {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day}, ${date} ${month} ${year}`;
+};
+
+const defaults = {
+  color: "white",
+  size: 112,
+  animate: true,
+};
+
+const Weather = () => {
+  const [weatherData, setWeatherData] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const getWeather = async (lat, lon) => {
+    try {
+      const api_call = await fetch(
+        `${apiKey.base}weather?lat=${lat}&lon=${lon}&units=metric&APPID=${apiKey.key}`
+      );
+      const data = await api_call.json();
+      setWeatherData({
+        city: data.name,
+        temperatureC: Math.round(data.main.temp),
+        temperatureF: Math.round(data.main.temp * 1.8 + 32),
+        humidity: data.main.humidity,
+        main: data.weather[0].main,
+        country: data.sys.country,
+      });
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (navigator.geolocation) {
+        try {
+          const position = await getPosition();
+          getWeather(position.coords.latitude, position.coords.longitude);
+        } catch (err) {
+          getWeather(28.67, 77.22);
+          alert(
+            "You have disabled location service. Allow 'This APP' to access your location. Your current location will be used for calculating Real time weather."
+          );
+        }
+      } else {
+        alert("Geolocation not available");
+      }
+
+      const timerID = setInterval(() => {
+        getWeather(weatherData.lat, weatherData.lon);
+      }, 600000);
+
+      return () => clearInterval(timerID);
+    };
+
+    fetchData();
+  }, []);
+
+  const getPosition = (options) => {
+    return new Promise(function (resolve, reject) {
+      navigator.geolocation.getCurrentPosition(resolve, reject, options);
+    });
+  };
+
+  if (loading) {
+    return (
+      <React.Fragment>
+        <img src={loader} style={{ width: "50%", WebkitUserDrag: "none" }} />
+        <h3 style={{ color: "white", fontSize: "22px", fontWeight: "600" }}>
+          Detecting your location
+        </h3>
+        <h3 style={{ color: "white", marginTop: "10px" }}>
+          Your current location wil be displayed on the App <br></br> & used
+          for calculating Real time weather.
+        </h3>
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <div className="city">
+          <div className="title">
+            <h2>{weatherData.city}</h2>
+            <h3>{weatherData.country}</h3>
+          </div>
+          <div className="mb-icon">
+            {" "}
+            <ReactAnimatedWeather
+              icon="CLEAR_DAY"
+              color={defaults.color}
+              size={defaults.size}
+              animate={defaults.animate}
+            />
+            <p>{weatherData.main}</p>
+          </div>
+          <div className="date-time">
+            <div className="dmy">
+              <div id="txt"></div>
+              <div className="current-time">
+                <Clock format="HH:mm:ss" interval={1000} ticking={true} />
+              </div>
+              <div className="current-date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="temperature">
+              <p>
+                {weatherData.temperatureC}°<span>C</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <Forecast icon="CLEAR_DAY" weather={weatherData.main} />
+      </React.Fragment>
+    );
+  }
+};
+
+export default Weather;
